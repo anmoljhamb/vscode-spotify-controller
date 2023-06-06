142d894af2f41db2bdf579c2470f5e62efb6cac9
Anmol Jhamb <talktoanmol@outlook.com>
2023-06-06 14:23:12 +0530

    removed the configurations of cleint_secret, and port

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,11 @@
 import * as vscode from "vscode";
 
 export const appId = "spotify-controller";
-export const PORT: number = vscode.workspace
+export const PORT = 61234;
+export const BACKEND_URI = `https://vscode-spotify-controller-server.vercel.app`;
+export const CLIENT_ID: string = vscode.workspace
     .getConfiguration()
-    .get("spotifyControllerApiPort") as number;
-export const BACKEND_URI = `http://localhost:8080`;
-export const CLIENT_ID = "1ecb3b66444c48918e9a3c282a18ab01";
+    .get("1ecb3b66444c48918e9a3c282a18ab01") as string;
 export const SHOW_ALERTS: boolean = vscode.workspace
     .getConfiguration()
     .get("spotifyControllerShowInformationAlerts") as boolean;
